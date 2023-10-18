acf00b97d5e712ad0e8f45022e32da8f5b1e0564
Sasikant Biswal <biswalsashikant@gmail.com>
2023-10-18 19:10:27 +0530

    Admin login component and redux setup

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,14 +1,28 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
+import store from "./store/index";
 const App = lazy(() => import("./App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Suspense fallback="loading...">
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback="loading...">
+        <App />
+        <Toaster
+          toastOptions={{
+            position: "top-right",
+            style: {
+              backgroundColor: "#283046",
+              color: "white",
+            },
+          }}
+        />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 );
---
+++ dashboard/src/store/Reducers/authReducer.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../api/api';
+
+const initialState = {
+    successMessage: "",
+    errrorMessage: "",
+    loader: false,
+    userInfo: "",
+}
+
+export const admin_login = createAsyncThunk(
+    "auth/admin_login",
+    async (info) => {
+        console.log(info);
+        try {
+            // const {data} = await api.post("/admin/login", info, {withCredentials: true});
+            // console.log(data);
+        } catch (error) {
+            
+        }
+    }
+)
+
+export const authReducer = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: {
+
+  }
+});
+
+// export const {} = authReducer.actions
+
+export default authReducer.reducer;
