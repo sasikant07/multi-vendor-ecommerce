2f5ab9451942891629715bf68b983cd6bc5ddcb5
Sasikant Biswal <biswalsashikant@gmail.com>
2023-11-18 00:42:07 +0530

    Added client setup

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Headers from '../components/Headers';
+
+const Home = () => {
+  return (
+    <div className='w-full'>
+        <Headers />
+    </div>
+  )
+}
+
+export default Home;
