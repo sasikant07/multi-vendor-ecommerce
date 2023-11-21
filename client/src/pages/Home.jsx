c5d96910e1c073c259c504cfdf29923ac66b4ebc
Sasikant Biswal <biswalsashikant@gmail.com>
2023-11-21 21:50:20 +0530

    Customer homepage banner and categories design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Headers from '../components/Headers';
+import Banner from '../components/Banner';
+import Categories from '../components/Categories';
 
 const Home = () => {
   return (
     <div className='w-full'>
         <Headers />
+        <Banner />
+        <div className="my-4">
+          <Categories />
+        </div>
     </div>
   )
 }
