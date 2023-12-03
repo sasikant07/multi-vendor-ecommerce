4dc1a25b68795c54fef4723ac9a435124c32a412
Sasikant Biswal <biswalsashikant@gmail.com>
2023-12-04 00:17:30 +0530

    User ratings and reviews design

---
+++ client/src/components/RatingTemp.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import { CiStar } from "react-icons/ci";
+
+const RatingTemp = ({ rating }) => {
+  if (rating === 5) {
+    return (
+      <>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+      </>
+    );
+  } else if (rating === 4) {
+    return (
+      <>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+      </>
+    );
+  } else if (rating === 3) {
+    return (
+      <>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+      </>
+    );
+  } else if (rating === 2) {
+    return (
+      <>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+      </>
+    );
+  } else if (rating === 1) {
+    return (
+      <>
+        <span className="text-[#EDBB0E]">
+          <AiFillStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+        <span className="text-[#EDBB0E]">
+          <CiStar />
+        </span>
+      </>
+    );
+  }
+};
+
+export default RatingTemp;
