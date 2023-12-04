2416159ebd99ff5e618c04b8a7a2fc239b00c4cf
Sasikant Biswal <biswalsashikant@gmail.com>
2023-12-04 18:53:05 +0530

    Added customer register login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,15 @@ import Home from "./pages/Home";
 import Shops from "./pages/Shops";
 import Cart from "./pages/Cart";
 import Details from "./pages/Details";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route  path="/register" element={<Register />}/>
+        <Route  path="/login" element={<Login />}/>
         <Route  path="/" element={<Home />}/>
         <Route  path="/shops" element={<Shops />}/>
         <Route  path="/cart" element={<Cart />}/>
